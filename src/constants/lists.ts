89e725736f131672b9452be86ab9f46d510f1494
CryptoSmallStar <song.crypto.currency@gmail.com>
2022-05-06 22:56:13 +0900

    add myswap-token-list

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -14,6 +14,7 @@ export const DEFAULT_LIST_OF_LISTS: string[] = [
   // 'tokenlist.aave.eth',
   'https://www.coingecko.com/tokens_list/uniswap/defi_100/v_0_0_0.json',
   'https://app.tryroll.com/tokens.json',
+  'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json',
   //'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json',
   'https://defiprime.com/defiprime.tokenlist.json',
   'https://umaproject.org/uma.tokenlist.json'
